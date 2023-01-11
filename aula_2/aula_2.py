53cd3967315b24d2531ff72a9c0644b062ded535
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2023-01-11 16:20:42 -0300

    updating

--- aula_2/aula_2.py
+++ aula_2/aula_2.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 
 # Passo 1: Importar a base de dados
 tabela = pd.read_csv(r"C:\Users\isaias\Desktop\intensivao_de_python\aula_2\telecom_users.csv")
@@ -9,4 +10,15 @@ tabela = tabela.drop('Unnamed: 0', axis=1)
 
 # Passo 3: Tratamento de dados
 tabela['TotalGasto'] = pd.to_numeric(tabela['TotalGasto'], errors='coerce')
+
+tabela = tabela.dropna(how='all', axis=1)
+tabela = tabela.dropna(how='any', axis=0)
 print(tabela.info())
+
+# Passo 4: Análise inicial
+print(tabela['Churn'].value_counts())
+print(tabela['Churn'].value_counts(normalize=True).map('{:.1%}'.format))
+
+# Passo 5: Análise detalhada - descobrir a causa do cancelamento
+grafico = px.histogram(tabela, x='Aposentado', color='Churn')
+grafico.show()
