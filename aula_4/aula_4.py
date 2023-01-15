d06a922b68668264ad6310d438c77797d50ecaa5
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2023-01-15 11:25:19 -0300

    updating

--- aula_4/aula_4.py
+++ aula_4/aula_4.py
@@ -7,10 +7,10 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 
 tabela = pd.read_csv(r'C:\Users\isaias\Desktop\intensivao_de_python\aula_4\advertising.csv')
-#print(tabela)
+# print(tabela)
 
-sns.heatmap(tabela.corr(), cmap='Blues', annot=True)
-plt.show()
+# sns.heatmap(tabela.corr(), cmap='Blues', annot=True)
+# plt.show()
 
 y = tabela['Vendas']
 x = tabela[['TV', 'Radio', 'Jornal']]
@@ -24,7 +24,18 @@ modelo_regressaolinear.fit(x_treino, y_treino)
 modelo_arvoredecisao.fit(x_treino, y_treino)
 
 previsao_regressaolinear = modelo_regressaolinear.predict(x_teste)
-previsao_arvoredecisao = modelo_arvoredecisao.preddict(x_teste)
+previsao_arvoredecisao = modelo_arvoredecisao.predict(x_teste)
 
 print(r2_score(y_teste, previsao_regressaolinear))
 print(r2_score(y_teste, previsao_arvoredecisao))
+
+tabela_auxiliar = pd.DataFrame()
+tabela_auxiliar['y_teste'] = y_teste
+tabela_auxiliar['Previsão Arvore Decisão'] = previsao_arvoredecisao
+tabela_auxiliar['Previsão Regressão Linear'] = previsao_regressaolinear
+
+sns.lineplot(data=tabela_auxiliar)
+# plt.show()
+
+nova_tabela = pd.read_csv('novos.csv')
+print(nova_tabela)
