b45cd0ff656382e1bbbb6782d3b99c1731ce4abf
Isaías Felipe Silva de Sousa <isaiasfelipe232@gmail.com>
2023-01-15 11:03:09 -0300

    updating

---
+++ aula_4/aula_4.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+
+tabela = pd.read_csv(r'C:\Users\isaias\Desktop\intensivao_de_python\aula_4\advertising.csv')
+#print(tabela)
+
+sns.heatmap(tabela.corr(), cmap='Blues', annot=True)
+plt.show()
+
+y = tabela['Vendas']
+x = tabela[['TV', 'Radio', 'Jornal']]
+
+x_treino, x_teste, y_treino, y_teste = train_test_split(x, y, test_size=0.3, random_state=1)
+
+modelo_regressaolinear = LinearRegression()
+modelo_arvoredecisao = RandomForestRegressor()
+
+modelo_regressaolinear.fit(x_treino, y_treino)
+modelo_arvoredecisao.fit(x_treino, y_treino)
+
+previsao_regressaolinear = modelo_regressaolinear.predict(x_teste)
+previsao_arvoredecisao = modelo_arvoredecisao.preddict(x_teste)
+
+print(r2_score(y_teste, previsao_regressaolinear))
+print(r2_score(y_teste, previsao_arvoredecisao))
